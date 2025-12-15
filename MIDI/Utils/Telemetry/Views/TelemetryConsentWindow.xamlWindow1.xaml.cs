742b74fb5d721582d4cfc9c9a260d6306837680e
routersys <machikado.mazoku.2@gmail.com>
2025-12-15 20:46:27 +0900

    UIの変更

--- MIDI/Utils/Telemetry/Views/TelemetryConsentWindow.xamlWindow1.xaml.cs
+++ MIDI/Utils/Telemetry/Views/TelemetryConsentWindow.xamlWindow1.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using MIDI.Utils.Telemetry.ViewModels;
 
 namespace MIDI.Utils.Telemetry.Views
@@ -12,6 +13,24 @@ namespace MIDI.Utils.Telemetry.Views
                 this.DialogResult = true;
                 this.Close();
             });
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
